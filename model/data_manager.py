254bd32859a7d5c3b570646cb6e5a7f9de4592cc
adrem01 <lukaszkedziora>
2019-12-12 18:43:55 +0100

    read nad save from file_ver_1

--- model/data_manager.py
+++ model/data_manager.py
@@ -1,5 +1,4 @@
-def get_table_from_file(file_name):
-    """
+"""
     Reads csv file and returns it as a list of lists.
     Lines are rows columns are separated by ","
 
@@ -8,12 +7,19 @@ def get_table_from_file(file_name):
 
     Returns:
          list: List of lists read from a file.
-    """
-    
+"""
 
 
-def write_table_to_file(file_name, table):
-    """
+def get_table_from_file(file_name):
+    with open(file_name, 'r') as file:
+        result = []
+        for line in file:
+            columns = line.split(',')
+            result.append(columns)
+    return result
+
+
+"""
     Writes list of lists into a csv file.
 
     Args:
@@ -22,4 +28,14 @@ def write_table_to_file(file_name, table):
 
     Returns:
          None
-    """
+"""
+
+
+def write_table_to_file(file_name, table):
+    with open(file_name, 'w') as file:
+        for line in table:
+            file.write(','.join([str(x) for x in line]))           
+    return
+
+#get_table_from_file('games.csv')
+#write_table_to_file('games2.csv', get_table_from_file('games.csv'))
