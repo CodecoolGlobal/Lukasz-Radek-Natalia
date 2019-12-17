c2f664ff16bf2913566d5821a46ead639894fd6c
adrem01 <lukaszkedziora>
2019-12-17 20:51:42 +0100

    printing_all_v1

--- view/terminal_view.py
+++ view/terminal_view.py
@@ -1,27 +1,7 @@
 """ Terminal view module """
 
- """
-    Prints table with data.
 
-    Example:
-        /-----------------------------------\
-        |   id   |      title     |  type   |
-        |--------|----------------|---------|
-        |   0    | Counter strike |    fps  |
-        |--------|----------------|---------|
-        |   1    |       fo       |    fps  |
-        \-----------------------------------/
-
-    Args:
-        table (list): list of lists - table to display
-        title_list (list): list containing table headers
-
-    Returns:
-        None: This function doesn't return anything it only prints to console.
-    """
 def print_table(table, title_list):
-
-  def print_table(table, title_list):
     print('-'* 124)
     for line in table:
         print('|', line[0].ljust(8," "), '|', line[1].ljust(40," "), '|',
@@ -30,66 +10,23 @@ def print_table(table, title_list):
 
 
 def print_result(result, label):
-    """
-    Displays results of the special functions.
-
-    Args:
-        result: result of the special function (string, list or dict)
-        label (str): label of the result
-
-    Returns:
-        None: This function doesn't return anything it only prints to console.
-    """
-
-    pass
-
+    print('The respult of', label, 'is', result)
+  
 
 def print_menu(title, list_options, exit_message):
-    """
-    Displays a menu. Sample output:
-        Main menu:
-            (1) Store manager
-            (2) Human resources manager
-            (3) Inventory manager
-            (4) Accounting manager
-            (5) Sales manager
-            (6) Customer relationship management (CRM)
-            (0) Exit program
-
-    Args:
-        title (str): menu title
-        list_options (list): list of strings - options that will be shown in menu
-        exit_message (str): the last option with (0) (example: "Back to main menu")
-
-    Returns:
-        None: This function doesn't return anything it only prints to console.
-    """
-
-    pass
+  print(title)
+  i = 0
+  for line in list_options:
+    i = i+1
+    print(i, line)
 
 
 def get_inputs(list_labels, title):
-    """
-    Gets list of inputs from the user.
-    Sample call:
-        get_inputs(["Name","Surname","Age"],"Please provide your personal information")
-    Sample display:
-        Please provide your personal information
-        Name <user_input_1>
-        Surname <user_input_2>
-        Age <user_input_3>
-
-    Args:
-        list_labels (list): labels of inputs
-        title (string): title of the "input section"
-
-    Returns:
-        list: List of data given by the user. Sample return:
-            [<user_input_1>, <user_input_2>, <user_input_3>]
-    """
-    inputs = []
-
-    pass
+    print(title)
+    answers = []
+    for line in list_labels:
+        answers.append(input(line))
+    return answers
 
 
 def get_choice(options):
@@ -99,14 +36,6 @@ def get_choice(options):
 
 
 def print_error_message(message):
-    """
-    Displays an error message (example: ``Error: @message``)
+    print('Error:', message)
 
-    Args:
-        message (str): error message to be displayed
-
-    Returns:
-        None: This function doesn't return anything it only prints to console.
-    """
-
-    pass
+    
