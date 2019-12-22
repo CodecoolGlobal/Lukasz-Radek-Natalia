6feac595c04d5794eff8d22257849b23d00b4051
en-kaa <natalia.krygier@gmail.com>
2019-12-22 11:17:27 +0100

    amended sales controller

--- controller/sales_controller.py
+++ controller/sales_controller.py
@@ -8,10 +8,30 @@ def run():
     options = ["Print list",
                "Add record",
                "Update record",
-               "Remove record",
-               'Find by ID/name/e-mail']
+               "Remove record"]
     
-pass
+   
+
+    list_labels = ['ID: ', 'Employee name: ', 'Customer name: ', 'Product ID: ', 'Number of items: ']
+=
+    sales_list = "sales/sales.csv"
+    
+
+
+    choice = None
+    while choice != "0":
+        choice = terminal_view.get_choice('SALES MENU', options)
+        if choice == "1":       # show list of all records
+            terminal_view.print_table(data_manager.get_table_from_file(sales_list), 'CRM list')
+        elif choice == "2":     # add record            
+            print("tralala")           
+        elif choice == "3":     # update record
+            print("tralala")    
+        elif choice == "4":     # remove record
+            print("tralala")    
+        else:
+            terminal_view.print_error_message("There is no such choice.")
+
 
 
 
