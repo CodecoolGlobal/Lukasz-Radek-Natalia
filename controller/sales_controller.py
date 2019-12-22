bb9aff651c07066586edadb85c57334854ae77cd
Radek Szczepanik <radekszczepanik.rsz@gmail.com>
2019-12-22 14:14:17 +0100

    aktualny plik

--- controller/sales_controller.py
+++ controller/sales_controller.py
@@ -3,20 +3,21 @@ from view import terminal_view
 from model.crm import crm
 from model import data_manager
 
+temp = ['']
 
 def run():
     options = ["Print list",
                "Add record",
                "Update record",
-               "Remove record"]
+               "Remove record",
+               'Transaction by employee',
+               'Transaction by customer']
     
    
 
     list_labels = ['ID: ', 'Employee name: ', 'Customer name: ', 'Product ID: ', 'Number of items: ']
-=
-    sales_list = "sales/sales.csv"
-    
-
+    sales_list = data_manager.get_table_from_file('sales/sales.csv')
+   
 
     choice = None
     while choice != "0":
@@ -29,6 +30,10 @@ def run():
             print("tralala")    
         elif choice == "4":     # remove record
             print("tralala")    
+        elif choice == '5':
+            terminal_view.print_result(crm.read(sales_list, terminal_view.get_inputs(temp, 'ID:', '')), '')
+        elif choice == '6':
+            terminal_view.print_result(crm.read(sales_list, terminal_view.get_inputs(temp, 'ID:', '')), '')
         else:
             terminal_view.print_error_message("There is no such choice.")
 
